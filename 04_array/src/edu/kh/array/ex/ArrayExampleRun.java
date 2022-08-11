2bbfdda805fb4128c6823bb87bd58f55744f4158
YJuY <110653665+YJuY@users.noreply.github.com>
2022-08-11 22:18:38 +0900

    배열(08.11)

---
+++ 04_array/src/edu/kh/array/ex/ArrayExampleRun.java
@@ -0,0 +1,10 @@
+package edu.kh.array.ex;
+
+public class ArrayExampleRun {
+   public static void mian(String[] args) {
+      
+      ArrayExample arrayEx = new ArrayExample();
+      
+      arrayEx.ex1();
+   }
+}
