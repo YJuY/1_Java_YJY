8b8efde7a4e6e1e0624eb5561c6d2209d992461c
YJuY <110653665+YJuY@users.noreply.github.com>
2022-08-12 16:48:02 +0900

    배열(08.12)
    구조, 선언, 할당, 저장구조, 초기화, 복사(얕은,깊은)

--- 04_array/src/edu/kh/array/ex/ArrayExampleRun.java
+++ 04_array/src/edu/kh/array/ex/ArrayExampleRun.java
@@ -1,10 +1,21 @@
 package edu.kh.array.ex;
 
 public class ArrayExampleRun {
-   public static void mian(String[] args) {
+   public static void main(String[] args) {
       
       ArrayExample arrayEx = new ArrayExample();
       
-      arrayEx.ex1();
+//      arrayEx.ex1();
+//      arrayEx.ex2();
+//      arrayEx.ex3();
+//      arrayEx.ex4();
+//      arrayEx.ex5();
+//      arrayEx.ex6();
+//      arrayEx.ex7();
+//      arrayEx.ex8();
+//      arrayEx.createLttoNumber();
+//      arrayEx.ex9();
+//      arrayEx.ex10();
+      arrayEx.ex11();
    }
 }
