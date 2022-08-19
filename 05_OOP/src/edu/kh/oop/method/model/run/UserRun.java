d1f9cd328ab5ca12970772bbb7e49b5728aaebca
YJuY <110653665+YJuY@users.noreply.github.com>
2022-08-19 16:21:39 +0900

    메서드(08.19)

---
+++ 05_OOP/src/edu/kh/oop/method/model/run/UserRun.java
@@ -0,0 +1,12 @@
+package edu.kh.oop.method.model.run;
+
+import edu.kh.oop.method.model.view.UserView;
+
+public class UserRun {
+
+	public static void main(String[] args) {
+		UserView view = new UserView(); // UserView 객체 생성
+		
+		view.displayMenu();
+	}
+}
