560e2916165d1c2cc601232db11f8105d4498563
YJuY <110653665+YJuY@users.noreply.github.com>
2022-08-30 17:41:01 +0900

    사원관리 프로그램

---
+++ EmployeeManagement/src/edu/kh/employee/run/Run.java
@@ -0,0 +1,11 @@
+package edu.kh.employee.run;
+
+import edu.kh.employee.model.service.EmployeeService;
+import edu.kh.employee.view.EmployeeView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new EmployeeView().dispalyMenu();
+	}
+}
